a67fb9b11d709c908ea98298f53e974e4aa6e702
Olexiy-repin <repinlyoha@gmail.com>
2024-07-12 21:02:34 +0300

    classwork

--- js/03-methods.js
+++ js/03-methods.js
@@ -18,15 +18,19 @@
 // console.log(fn.test);
 
 //? Викличте функцію showFullName у контексті об'єкта user
-const showFullName = function () {
-  console.log(`${this.firstName} ${this.lastName}`);
-};
+// const showFullName = function (message, someNumber) {
+//   console.log(`${message} ${this.firstName} ${this.lastName}`);
+//   console.log(someNumber);
+// };
 
-const user = {
-  firstName: 'Ernest',
-  lastName: 'Vasquez',
-  age: 30,
-};
+// const user = {
+//   firstName: 'Ernest',
+//   lastName: 'Vasquez',
+//   age: 30,
+// };
+
+// showFullName.call(user, 'Welcome', 100);
+// showFullName.apply(user, ['Hello', 200]);
 
 //? Викличте функцію showFullName у контексті об'єкта anotherUser
 // const anotherUser = {
@@ -34,6 +38,8 @@ const user = {
 //   lastName: 'Jordan',
 //   age: 30,
 // };
+
+// showFullName.call(anotherUser, 'Hello', 300);
 
 //? Викличте функцію changeColor в контексті різних обʼєктів
 // const changeColor = function (color) {
@@ -46,9 +52,21 @@ const user = {
 //   color: 'black',
 // };
 
+// console.log(hat);
+
+// changeColor.call(hat, 'red');
+
+// console.log(hat);
+
 // const sweater = {
 //   color: 'green',
 // };
+
+// console.log(sweater);
+
+// changeColor.apply(sweater, ['yellow']);
+
+// console.log(sweater);
 
 //? Створіть копію функції changeColor з привʼязаним контекстом
 // const changeColor = function (color) {
@@ -59,9 +77,32 @@ const user = {
 //   color: 'black',
 // };
 
+// console.log(hat);
+
+/*
+function (color) {
+  this = hat;
+  this.color = color;
+};
+*/
+
+// const changeHatColor = changeColor.bind(hat);
+
+// changeHatColor('red');
+
+// console.log(hat);
+
 // const sweater = {
 //   color: 'green',
 // };
+
+// console.log(sweater);
+
+// const changeSweaterColor = changeColor.bind(sweater);
+
+// changeSweaterColor('yellow');
+
+// console.log(sweater);
 
 //? Передайте метод обʼєкта в якості callback функції
 // const user = {
@@ -81,4 +122,5 @@ const user = {
 //   callback();
 // };
 
-// someFunction(user.showName);
+// someFunction(user.showThis.bind(user));
+// someFunction(user.showName.bind(user));
--- js/04-prototype.js
+++ js/04-prototype.js
@@ -6,3 +6,59 @@
  * - Перебір власних властивостей з hasOwnProperty()
  * - Ланцюжки прототипів
  */
+
+// const userMethods = {
+//   getFullName() {
+//     console.log(`${this.firstName} ${this.lastName}`);
+//   },
+
+//   changeAge(newAge) {
+//     this.age = newAge;
+//   },
+// };
+
+// const userPauline = Object.create(userMethods);
+
+// userPauline.firstName = 'Pauline';
+// userPauline.lastName = 'George';
+// userPauline.age = 20;
+
+// console.log(userPauline);
+
+// userPauline.getFullName();
+
+// const userAnnie = Object.create(userMethods);
+
+// userAnnie.firstName = 'Annie';
+// userAnnie.lastName = 'Sims';
+// userAnnie.age = 22;
+
+// console.log(userAnnie);
+
+// userAnnie.getFullName();
+
+//* Цикл for...in
+
+const userMethods = {
+  getFullName() {
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+
+  changeAge(newAge) {
+    this.age = newAge;
+  },
+};
+
+const userPauline = Object.create(userMethods);
+
+userPauline.firstName = 'Pauline';
+userPauline.lastName = 'George';
+userPauline.age = 20;
+
+console.log(userPauline);
+
+for (const key in userPauline) {
+  if (userPauline.hasOwnProperty(key)) {
+    console.log(key);
+  }
+}
